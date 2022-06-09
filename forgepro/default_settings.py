8c21870035bc2e3b28aee6ed5de8092dfeac5058
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-06-09 12:11:26 -0500

    Add FORGEPRO_APPS and FORGEPRO_MIDDLEWARE settings

--- forgepro/default_settings.py
+++ forgepro/default_settings.py
@@ -4,3 +4,20 @@ from .requestlog.default_settings import *
 from .sentry.default_settings import *
 from .stafftoolbar.default_settings import *
 from .stripe.default_settings import *
+
+FORGEPRO_APPS = [
+    "forgepro.sentry",
+    "forgepro.stripe",
+    "forgepro.stafftoolbar",
+    "forgepro.impersonate",
+    "forgepro.googleanalytics",
+    "forgepro.requestlog",
+    "forgepro.querystats",
+]
+
+FORGEPRO_MIDDLEWARE = [
+    "forgepro.querystats.QueryStatsMiddleware",
+    "forgepro.sentry.SentryFeedbackMiddleware",
+    "forgepro.impersonate.ImpersonateMiddleware",
+    "forgepro.requestlog.RequestLogMiddleware",
+]
