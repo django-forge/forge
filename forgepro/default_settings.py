101b8b5260ab710de674b262f899a8041eb3aab2
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-06-09 11:31:40 -0500

    Add requestlog app

--- forgepro/default_settings.py
+++ forgepro/default_settings.py
@@ -1,5 +1,6 @@
 from .googleanalytics.default_settings import *
 from .impersonate.default_settings import *
+from .requestlog.default_settings import *
 from .sentry.default_settings import *
 from .stafftoolbar.default_settings import *
 from .stripe.default_settings import *
---
+++ forgepro/requestlog/default_settings.py
@@ -0,0 +1,7 @@
+REQUESTLOG_IGNORE_URL_PATHS = [
+    "/sw.js",
+    "/favicon.ico",
+    "/admin/jsi18n/",
+]
+REQUESTLOG_KEEP_LATEST = 50
+REQUESTLOG_URL = "/requestlog/"
